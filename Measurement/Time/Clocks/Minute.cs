cc2574fbd7aa5f205197957cc5fa46fe521cdeed
AIBrain <rick@aibrain.org>
2014-08-11 22:16:54 -0600

    cleaned up the logic

--- Measurement/Time/Clocks/Minute.cs
+++ Measurement/Time/Clocks/Minute.cs
@@ -1,99 +1,94 @@
 #region License & Information
+
 // This notice must be kept visible in the source.
-// 
-// This section of source code belongs to Rick@AIBrain.Org unless otherwise specified,
-// or the original license has been overwritten by the automatic formatting of this code.
-// Any unmodified sections of source code borrowed from other projects retain their original license and thanks goes to the Authors.
-// 
+//
+// This section of source code belongs to Rick@AIBrain.Org unless otherwise specified, or the
+// original license has been overwritten by the automatic formatting of this code. Any unmodified
+// sections of source code borrowed from other projects retain their original license and thanks
+// goes to the Authors.
+//
 // Donations and Royalties can be paid via
 // PayPal: paypal@aibrain.org
-// bitcoin:1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2
-// bitcoin:1NzEsF7eegeEWDr5Vr9sSSgtUC4aL6axJu
-// litecoin:LeUxdU2w3o6pLZGVys5xpDZvvo8DUrjBp9
-// 
-// Usage of the source code or compiled binaries is AS-IS.
-// I am not responsible for Anything You Do.
-// 
-// "Librainian/Minute.cs" was last cleaned by Rick on 2014/08/11 at 12:39 AM
-#endregion
+// bitcoin: 1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2
+// bitcoin: 1NzEsF7eegeEWDr5Vr9sSSgtUC4aL6axJu
+// litecoin: LeUxdU2w3o6pLZGVys5xpDZvvo8DUrjBp9
+//
+// Usage of the source code or compiled binaries is AS-IS. I am not responsible for Anything You Do.
+//
+// "Librainian/Minute.cs" was last cleaned by Rick on 2014/08/11 at 10:15 PM
+
+#endregion License & Information
 
 namespace Librainian.Measurement.Time.Clocks {
+
     using System;
     using System.Runtime.Serialization;
-    using System.Threading;
     using FluentAssertions;
+    using Librainian.Extensions;
 
     /// <summary>
-    ///     A simple struct for a <see cref="Minute" />.
+    /// A simple struct for a <see cref="Minute" />.
     /// </summary>
     [DataContract]
     [Serializable]
+    [Immutable]
     public struct Minute : IPartofaClock {
+
         /// <summary>
-        ///     60
+        /// 60
         /// </summary>
         public const Byte Maximum = Minutes.InOneHour;
 
         /// <summary>
-        ///     1
+        /// 1
         /// </summary>
         public const Byte Minimum = 1;
 
-        private long _value;
+        /// <summary>
+        /// </summary>
+        public static readonly Minute MaxMinute = new Minute( Maximum );
 
-        static Minute() {
-            Maximum.Should().BeGreaterThan( Minimum );
-        }
-
-        public Minute( Byte minute ) : this() {
-            this.Set( minute );
-        }
+        /// <summary>
+        /// </summary>
+        public static readonly Minute MinMinute = new Minute( Minimum );
 
         [DataMember]
-        public Byte Value {
-            get { return ( Byte ) Interlocked.Read( ref this._value ); }
-            set {
-                value.Should().BeInRange( Minimum, Maximum );
+        private readonly Byte _value;
 
-                if ( value < Minimum || value > Maximum ) {
-                    throw new ArgumentOutOfRangeException( "value", String.Format( "The specified minute {0} is out of the valid range {1} to {2}.", value, Minimum, Maximum ) );
-                }
-                Interlocked.Exchange( ref this._value, value );
-            }
+        public Minute( Byte minute ) {
+            Validate( minute );
+            this._value = minute;
+        }
+
+        public Minute( long minute ) {
+            Validate( minute );
+            this._value = ( Byte )minute;
         }
 
         /// <summary>
-        ///     <para>Decrease the current minute.</para>
-        ///     <para>Returns true if the value passed <see cref="Maximum" /></para>
+        /// Allow this class to be visibly cast to a <see cref="SByte" />.
         /// </summary>
-        public Boolean Rewind() {
-            var value = ( Int16 ) this.Value;
-            value--;
-            if ( value < Minimum ) {
-                this.Value = Maximum;
-                return true;
-            }
-            this.Value = ( Byte ) value;
-            return false;
-        }
-
-        public void Set( Byte value ) {
-            this.Value = value;
+        /// <param name="minute"></param>
+        /// <returns></returns>
+        public static explicit operator SByte( Minute minute ) {
+            return ( SByte )minute._value;
         }
 
         /// <summary>
-        ///     <para>Increase the current minute.</para>
-        ///     <para>Returns true if the value passed <see cref="Maximum" /></para>
+        /// Allow this class to be read as a <see cref="Byte" />.
         /// </summary>
-        public Boolean Tick() {
-            var value = this.Value;
-            value++;
-            if ( value > Maximum ) {
-                this.Value = Minimum;
-                return true;
+        /// <param name="minute"></param>
+        /// <returns></returns>
+        public static implicit operator Byte( Minute minute ) {
+            return minute._value;
+        }
+
+        private static void Validate( long minute ) {
+            minute.Should().BeInRange( Minimum, Maximum );
+
+            if ( minute < Minimum || minute > Maximum ) {
+                throw new ArgumentOutOfRangeException( "minute", String.Format( "The specified minute {0} is out of the valid range {1} to {2}.", minute, Minimum, Maximum ) );
             }
-            this.Value = value;
-            return false;
         }
     }
 }
