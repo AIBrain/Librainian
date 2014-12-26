6aea49e17d37f03fb6c44b1b5e527ca84879723d
AIBrain <rick@aibrain.org>
2014-12-26 16:10:07 -0700

    is this wrapper being coded correctly?

--- Internet/Browser/AwesomiumWrapper.cs
+++ Internet/Browser/AwesomiumWrapper.cs
@@ -25,46 +25,73 @@ namespace Librainian.Internet.Browser {
             Release();
         }
 
-        private async Task< Boolean > WaitAsync( TimeSpan timeout ) => await this._semaphore.WaitAsync( timeout );
+        private async Task<Boolean> WaitAsync( TimeSpan timeout ) => await this._semaphore.WaitAsync( timeout );
 
         private void Release() => this._semaphore.Release();
 
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim( initialCount: 0, maxCount: 1 );
 
-        [ CanBeNull ]
-        public async Task< object > RunFunction( String id, String function, TimeSpan timeout ) {
-            try {
-                var doWeHaveAccess = await WaitAsync( timeout );
-                if ( !doWeHaveAccess ) {
-                    return null;
+        public async Task<Boolean> ExecuteJavascript( String javascript, TimeSpan timeout, int retries = 1 ) {
+            var doWeHaveAccess = false;
+            while ( retries > 0 ) {
+                retries--;
+                try {
+                    if ( retries > 0 ) {
+                        doWeHaveAccess = await this.WaitAsync( timeout );
+                        if ( doWeHaveAccess ) {
+                            this.WebControl.Invoke( new Action( () => this.WebControl.ExecuteJavascript( javascript ) ) );
+                            return true;
+                        }
+                    }
                 }
-                var result = this.WebControl.Invoke( new Action( () => this.WebControl.ExecuteJavascript( String.Format( "document.getElementById( {0} ).{1}();", id, function ) ) ) );
-                return result;
-            }
-            catch ( Exception exception ) {
-                exception.More();
-            }
-            finally {
-                this.Release();
-            }
-            return null;
-        }
-
-        public async Task< bool > Value( String id, String text, TimeSpan timeout ) {
-            try {
-                var doWeHaveAccess = await WaitAsync( timeout );
-                if ( doWeHaveAccess ) {
-                    this.WebControl.Invoke( new Action( () => this.WebControl.ExecuteJavascript( String.Format( "document.getElementById( {0} ).value=\"{1}\";", id, text ) ) ) );
-                    return true;
+                catch ( Exception exception ) {
+                    exception.More();
                 }
-            }
-            catch ( Exception exception ) {
-                exception.More();
-            }
-            finally {
-                this.Release();
+                finally {
+                    if ( doWeHaveAccess ) {
+                        this.Release();
+                    }
+                }
             }
             return false;
+        }
+
+        public async Task<TResult> ExecuteJavascriptWithResult<TResult>( String javascript, TimeSpan timeout, int retries = 1 ) {
+            var doWeHaveAccess = false;
+            while ( retries > 0 ) {
+                retries--;
+                try {
+                    if ( retries > 0 ) {
+                        doWeHaveAccess = await this.WaitAsync( timeout );
+                        if ( doWeHaveAccess ) {
+                            var result = this.WebControl.Invoke( new Action( () => this.WebControl.ExecuteJavascriptWithResult( javascript ) ) ) ;
+                            return result is TResult ? ( TResult ) result : default( TResult );
+                        }
+                    }
+                }
+                catch ( Exception exception ) {
+                    exception.More();
+                }
+                finally {
+                    if ( doWeHaveAccess ) {
+                        this.Release();
+                    }
+                }
+            }
+            return default( TResult );
+        }
+
+        [CanBeNull]
+        public async Task<Boolean> ExecuteJavascriptMethod( String id, String function, TimeSpan timeout ) {
+            var javascript = String.Format( "document.getElementById( {0} ).{1}();", id, function );
+            var result = await this.ExecuteJavascript( javascript, timeout );
+            return result;
+        }
+
+        public async Task<Boolean> SetValue( String id, String text, TimeSpan timeout ) {
+            var javascript = String.Format( "document.getElementById( {0} ).value=\"{1}\";", id, text );
+            var result = await this.ExecuteJavascript( javascript, timeout );
+            return result;
         }
 
 
