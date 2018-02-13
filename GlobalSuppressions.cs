a02e12374974bfaeca05135d0f0f36354efb0e39
AIBrain <rick@aibrain.org>
2018-02-12 21:58:28 -0700

    little changes

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage( "Design", "CC0004:Catch block cannot be empty", Justification = "<Pending>", Scope = "member", Target = "~M:Librainian.FileSystem.Folder.TryParse(System.String,Librainian.FileSystem.Folder@)~System.Boolean" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage( "Design", "CC0004:Catch block cannot be empty", Justification = "<Pending>", Scope = "member", Target = "~M:Librainian.FileSystem.Folder.DemandPermission(System.Security.Permissions.FileIOPermissionAccess)~System.Boolean" )]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage( "Design", "CC0004:Catch block cannot be empty", Justification = "<Pending>", Scope = "member", Target = "~M:Librainian.FileSystem.Folder.Delete~System.Boolean" )]
+
