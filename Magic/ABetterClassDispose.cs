e3289930fb34447ad31febf7627ec9c01dea3f3a
Protiguous <Protiguous@Protiguous.com>
2018-05-13 01:19:23 -0600

    Made ABetterClassDispose.cs a little better, I hope.

--- Magic/ABetterClassDispose.cs
+++ Magic/ABetterClassDispose.cs
@@ -1,85 +1,82 @@
-﻿// Copyright 2017 Protiguous.
-// 
+﻿// Copyright 2018 Protiguous.
+//
 // This notice must be kept visible in the source.
-// 
-// This section of source code belongs to Protiguous@Protiguous.com unless otherwise specified, or the
-// original license has been overwritten by the automatic formatting of this code. Any unmodified
-// sections of source code borrowed from other projects retain their original license and thanks
-// goes to the Authors.
-// 
-// Donations and royalties can be paid via
-//  
-//  bitcoin: 1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2
-//  
-// 
+//
+// This section of source code belongs to Protiguous@Protiguous.com unless otherwise specified or the original license has been overwritten by the automatic formatting of this code.
+//
+// Any unmodified sections of source code borrowed from other projects retain their original license and thanks goes to the Authors.
+//
+// Donations, royalties, and licenses can be paid via bitcoin: 1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2
+//
 // Usage of the source code or compiled binaries is AS-IS. I am not responsible for Anything You Do.
-// 
+//
 // Contact me by email if you have any questions or helpful criticism.
-// 
-// "Librainian/ABetterClassDispose.cs" was last cleaned by Protiguous on 2017/12/24 at 8:30 AM
+//
+// "Librainian/ABetterClassDispose.cs" was last cleaned by Protiguous on 2018/05/13 at 1:17 AM
 
 namespace Librainian.Magic {
-	using System;
-	using System.Diagnostics;
 
-	/// <summary>
-	///     <para>A better class for implementing the <see cref="IDisposable" /> pattern.</para>
-	///     <para>Implement <see cref="DisposeManaged" /> and <see cref="DisposeNative" />.</para>
-	/// </summary>
-	/// <remarks>ABCD hehe. Written by Rick Harker</remarks>
-	public class ABetterClassDispose : IDisposable {
+    using System;
 
-		public Boolean HasDisposedManaged { get; private set; }
-		public Boolean HasDisposedNative { get; private set; }
+    /// <summary>
+    /// <para>A better class for implementing the <see cref="IDisposable"/> pattern.</para>
+    /// <para>Implement <see cref="DisposeManaged"/> and <see cref="DisposeNative"/>.</para>
+    /// </summary>
+    /// <remarks>ABCD hehe. Written by Rick Harker</remarks>
+    public class ABetterClassDispose : IDisposable {
 
-		public void Dispose() {
-			this.Dispose( disposing: true );
-			GC.SuppressFinalize( this );
-		}
+        ~ABetterClassDispose() => this.Dispose( disposing: false );
 
-	    private void Dispose( Boolean disposing ) {
-			if ( !disposing ) {
-				return;
-			}
+        public Boolean HasDisposedManaged { get; private set; }
 
-			if ( !this.HasDisposedManaged ) {
-				try {
-					this.DisposeManaged();
-				}
+        public Boolean HasDisposedNative { get; private set; }
 
-				catch ( Exception exception) {
-				    exception.Break();
-				}
-				finally {
-					this.HasDisposedManaged = true;
-				}
-			}
+        private void Dispose( Boolean disposing ) {
 
-			if ( !this.HasDisposedNative ) {
-				try {
-					this.DisposeNative();
-				}
-
-				catch ( Exception exception ) {
-				    exception.Break();
-				}
-
+            if ( !this.HasDisposedManaged ) {
+                try {
+                    this.DisposeManaged();
+                }
+                catch ( Exception exception ) {
+                    exception.Break();
+                }
                 finally {
-					this.HasDisposedNative = true;
-				}
-			}
-		}
+                    this.HasDisposedManaged = true;
+                }
+            }
 
-		/// <summary>
-		///     Dispose any disposable managed fields or properties.
-		/// </summary>
-		public virtual void DisposeManaged() { }
+            if ( !this.HasDisposedNative ) {
+                try {
+                    this.DisposeNative();
+                }
+                catch ( Exception exception ) {
+                    exception.Break();
+                }
+                finally {
+                    this.HasDisposedNative = true;
+                }
+            }
 
-		/// <summary>
-		///     Dispose of COM objects, Handles, etc. Then set those objects to null.
-		/// </summary>
-		public virtual void DisposeNative() { }
+            if ( !disposing ) { }
+        }
 
-		~ABetterClassDispose() => this.Dispose( disposing: false );
-	}
+        public void Dispose() {
+            this.Dispose( disposing: true );
+            GC.SuppressFinalize( this );
+        }
+
+        /// <summary>
+        /// Dispose any disposable managed fields or properties.
+        /// </summary>
+        public virtual void DisposeManaged() {
+            this.HasDisposedManaged = true;
+        }
+
+        /// <summary>
+        /// Dispose of COM objects, Handles, etc. Then set those objects to null if possible.
+        /// </summary>
+        public virtual void DisposeNative() {
+            this.HasDisposedNative = true;
+        }
+    }
 }
