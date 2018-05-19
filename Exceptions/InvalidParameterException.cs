18aaef220549d88fde5e4da0dab108eb54f8c833
Protiguous <Protiguous@Protiguous.com>
2018-05-19 00:11:34 -0600

    Created some new Exceptions and Warnings.

---
+++ Exceptions/InvalidParameterException.cs
@@ -0,0 +1,54 @@
+﻿// Copyright © 1995-2018 to Rick@AIBrain.org and Protiguous. All Rights Reserved.
+//
+// This entire copyright notice and license must be retained and must be kept visible
+// in any binaries, libraries, repositories, and source code (directly or derived) from
+// our binaries, libraries, projects, or solutions.
+//
+// This source code, "InvalidParameterException.cs", belongs to Rick@AIBrain.org and Protiguous@Protiguous.com
+// unless otherwise specified or the original license has been overwritten by automatic formatting.
+// (We try to avoid it from happening, but it does accidentally happen.)
+//
+// Any unmodified portions of source code gleaned from other projects still retain their original
+// license and our thanks goes to those Authors. If you find your code in this source code, please
+// let us know so we can properly attribute you and include the proper license and/or copyright.
+//
+// Donations, royalties from any software that uses any of our code, or license fees can be paid
+// to us via bitcoin at the address 1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2.
+//
+// =========================================================
+// Usage of the source code or binaries is AS-IS.
+// No warranties are expressed, implied, or given.
+// We are NOT responsible for Anything You Do With Our Code.
+// =========================================================
+//
+// Contact us by email if you have any questions, helpful criticism, or if you would like to use our code in your project(s).
+//
+// "Librainian/Librainian/InvalidParameterException.cs" was last formatted by Protiguous on 2018/05/18 at 11:22 PM.
+
+namespace Librainian.Exceptions {
+
+    using System;
+    using JetBrains.Annotations;
+
+    /// <summary>
+    ///     Use when a parameter is, well.. invalid.
+    /// </summary>
+    [Serializable]
+    public class InvalidParameterException : ArgumentException {
+
+        /// <summary>
+        ///     Disallow no message.
+        /// </summary>
+        private InvalidParameterException() { }
+
+        public InvalidParameterException( [NotNull] String message ) : base( message ) {
+
+            //if ( message.IsNullOrEmpty() ) { throw new NotAllowed( "A message must be provided." ); }
+        }
+
+        public InvalidParameterException( [NotNull] String message, Exception inner ) : base( message, inner ) {
+
+            //if ( message.IsNullOrEmpty() ) { throw new NotAllowed( "A message must be provided." ); }
+        }
+    }
+}
---
+++ Exceptions/OutOfRangeException.cs
@@ -0,0 +1,52 @@
+﻿// Copyright © 1995-2018 to Rick@AIBrain.org and Protiguous. All Rights Reserved.
+//
+// This entire copyright notice and license must be retained and must be kept visible
+// in any binaries, libraries, repositories, and source code (directly or derived) from
+// our binaries, libraries, projects, or solutions.
+//
+// This source code, "OutOfRangeException.cs", belongs to Rick@AIBrain.org and Protiguous@Protiguous.com
+// unless otherwise specified or the original license has been overwritten by automatic formatting.
+// (We try to avoid it from happening, but it does accidentally happen.)
+//
+// Any unmodified portions of source code gleaned from other projects still retain their original
+// license and our thanks goes to those Authors. If you find your code in this source code, please
+// let us know so we can properly attribute you and include the proper license and/or copyright.
+//
+// Donations, royalties from any software that uses any of our code, or license fees can be paid
+// to us via bitcoin at the address 1Mad8TxTqxKnMiHuZxArFvX8BuFEB9nqX2.
+//
+// =========================================================
+// Usage of the source code or binaries is AS-IS.
+// No warranties are expressed, implied, or given.
+// We are NOT responsible for Anything You Do With Our Code.
+// =========================================================
+//
+// Contact us by email if you have any questions, helpful criticism, or if you would like to use our code in your project(s).
+//
+// "Librainian/Librainian/OutOfRangeException.cs" was last formatted by Protiguous on 2018/05/18 at 11:24 PM.
+
+namespace Librainian.Exceptions {
+
+    using System;
+    using Parsing;
+
+    /// <summary>
+    ///     Use when a value is out of range. (Too low or too high)
+    /// </summary>
+    [Serializable]
+    public class OutOfRangeException : Exception {
+
+        /// <summary>
+        ///     Disallow no message.
+        /// </summary>
+        private OutOfRangeException() { }
+
+        public OutOfRangeException( String message ) : base( message ) {
+            if ( message.IsNullOrEmpty() ) { throw new NotAllowed( "A message must be provided." ); }
+        }
+
+        public OutOfRangeException( String message, Exception inner ) : base( message, inner ) {
+            if ( message.IsNullOrEmpty() ) { throw new NotAllowed( "A message must be provided." ); }
+        }
+    }
+}
